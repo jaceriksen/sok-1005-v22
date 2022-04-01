c9231bc7360834844d8472c8aa4b20ab712d73a7
jaceriksen <90839491+jaceriksen@users.noreply.github.com>
2022-04-01 12:28:22 +0200

    Create strategy.py

---
+++ oppgave5/strategy.py
@@ -0,0 +1,12 @@
+def player_strategy(n_battalions,n_fields):
+    
+    battalions=np.zeros(n_fields,dtype=int)
+    
+    battalions[0:3]=31
+    battalions[3:5]=2
+    battalions[5:]=3
+    
+    battalions=battalions[np.random.rand(n_fields).argsort()]
+    assert sum(battalions)==n_battalions
+    
+    return battalions
